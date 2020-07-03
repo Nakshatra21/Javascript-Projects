907e9e54d212f343eea9866a9eec17771f6fdb53
Naveen <nava.srmece@gmail.com>
2020-07-03 22:42:46 +0530

    Updated script file and removed if else clause
    replaced if else condition into a better code

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -4,6 +4,35 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+
+//getFieldName method
+
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//checkRequired method
+
+function checkRequired(arr){
+    arr.forEach(function(input){
+        if(input.value === ''){
+            showError(input, `${getFieldName(input)} is required`);
+        }else{
+            showSuccess(input);
+        }
+    });
+}
+//check length
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} should have minimum ${min} characters`);
+    }else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} should only have maximum ${max} characters`);
+    }else{
+        showSuccess(input);
+    }
+}
+
 // show input error messages
 
 function showError(input, message){
@@ -11,6 +40,13 @@ function showError(input, message){
     formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
     small.innerText = message;
+}
+
+//check passwords match
+function checkPasswordMatch(password, password2){
+    if(password.value !== password2.value){
+        showError(password2, "Passwords do not match");
+    }
 }
 
 // show success outline
@@ -22,55 +58,25 @@ function showSuccess(input, message){
 }
 
 // function to validate email
-function isValidEmail(input){
+function checkEmail(input){
     // const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
    
-    return re.test(String(input.value).toLowerCase());
+    if(!re.test(String(input.value).toLowerCase())){
+        showError(input, 'Email is not valid');
+    }else{
+        showSuccess(input);
+    }
 }
 
 // Adding Event Listener for submit event
 form.addEventListener('submit', function(e){
     // preventing default submit action before validation 
-     e.preventDefault();
-     
-     //username field
-    if(username.value === ''){
-        showError(username, 'Username is required');
-     }else {
-         showSuccess(username);
-         
-     }
-
-     //email field
-     if(email.value === ''){
-        showError(email, 'Email is required');
-     }else if(!isValidEmail(email)){
-        showError(email, 'Email is not valid');
-     }else{
-         showSuccess(email);
-     }
-
-     //password field
-     if(password.value === ''){
-        showError(password, 'Password is required');
-     }else {
-         showSuccess(password);
-     }
-
-    // confirm password field
-     if(password2.value === ''){
-        showError(password2, 'Password 2 is required');
-     }
-
-    // Password Match
-     if(password.value !== '' &&  password2.value !== ''){
-        
-        if(password.value === password2.value){
-            showSuccess(password2); 
-         }
-        else{
-             showError(password2,'Passwords do not match');
-        }
-    }    
+    e.preventDefault();
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 12);
+    checkLength(password2, 6, 12);
+    checkEmail(email);
+    checkPasswordMatch(password, password2);
 });
